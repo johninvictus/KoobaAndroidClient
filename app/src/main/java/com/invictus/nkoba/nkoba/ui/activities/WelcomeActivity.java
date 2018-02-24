0a5e2217e7acaa44fa0884b3635ef6349fe5cacf
johninvictus <invictusnitude@gmail.com>
2018-02-24 15:03:59 +0300

    added dagger dependencies

--- app/src/main/java/com/invictus/nkoba/nkoba/ui/activities/WelcomeActivity.java
+++ app/src/main/java/com/invictus/nkoba/nkoba/ui/activities/WelcomeActivity.java
@@ -5,12 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberUtils;
+import android.widget.Toast;
 
 import com.andexert.library.RippleView;
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.PhoneNumber;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
 import com.invictus.nkoba.nkoba.R;
+
+import java.util.Locale;
+
+import timber.log.Timber;
 
 /**
  * Created by invictus on 1/5/18.
@@ -27,7 +42,7 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         rippleView = findViewById(R.id.login_register_btn);
-        rippleView.setOnRippleCompleteListener(rippleView1 -> onLogin(LoginType.EMAIL));
+        rippleView.setOnRippleCompleteListener(rippleView1 -> onLogin(LoginType.PHONE));
     }
 
 
@@ -42,7 +57,7 @@ public class WelcomeActivity extends AppCompatActivity {
         AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
                 new AccountKitConfiguration.AccountKitConfigurationBuilder(
                         loginType,
-                        AccountKitActivity.ResponseType.TOKEN);
+                        AccountKitActivity.ResponseType.CODE);
 
         final AccountKitConfiguration configuration = configurationBuilder.build();
 
@@ -50,5 +65,68 @@ public class WelcomeActivity extends AppCompatActivity {
         intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configuration);
 
         startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == APP_REQUEST_CODE) {
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (loginResult.getError() != null) {
+                String errorStr = loginResult.getError().getErrorType().getMessage();
+                Toast.makeText(this, errorStr, Toast.LENGTH_SHORT).show();
+            }else if(loginResult.wasCancelled()){
+                String toastMessage = "Login Canceled";
+                Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+            }else {
+                String authCode = loginResult.getAuthorizationCode();
+
+                //send this token to my server
+                Timber.e(authCode);
+            }
+        }
+    }
+
+    // client authorization
+    private void afterLogin() {
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+                // grab all your data
+                // #get account id
+                String accountId = account.getId();
+
+                // number
+                PhoneNumber phoneNumber = account.getPhoneNumber();
+                if (phoneNumber != null) {
+                    String number = formattedNumber(phoneNumber.toString());
+                } else {
+                    // use the email
+                }
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+                //show error toast
+                // send data to crashlytics
+                String errorString = accountKitError.getErrorType().getMessage();
+                Toast.makeText(WelcomeActivity.this, errorString, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String formattedNumber(String phoneNumber) {
+
+        try {
+            PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+            Phonenumber.PhoneNumber pn = pnu.parse(phoneNumber, Locale.getDefault().getCountry());
+
+            phoneNumber = pnu.format(pn, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
+        } catch (NumberParseException e) {
+            e.printStackTrace();
+        }
+
+        return phoneNumber;
     }
 }
